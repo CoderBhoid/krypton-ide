81f0432cd725ac5a003df64e62f52c527b01c1b2
Bhoidy <officialbhoid@gmail.com>
2026-04-07 05:17:38 +0530

    Change appId from 'com.sednium.krypton' to 'com.sednium.kryptonide'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.sednium.krypton',
+  appId: 'com.sednium.kryptonide',
   appName: 'Krypton IDE',
   webDir: 'dist',
   
