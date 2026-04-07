499db3d12428433ea1c905a3e75ba3f581739065
Bhoidy <officialbhoid@gmail.com>
2026-04-07 05:38:23 +0530

    Implement storage permission flow in MainActivity
    Add storage permission check for Android 11 and above.

--- android/app/src/main/java/com/sednium/krypton/MainActivity.java
+++ android/app/src/main/java/com/sednium/krypton/MainActivity.java
@@ -1,6 +1,11 @@
 package com.sednium.kryptonide;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +13,20 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(ApkInstallerPlugin.class);
+        
+        // Trigger the professional permission flow
+        checkStoragePermission();
+    }
+
+    private void checkStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        }
     }
 }
+
